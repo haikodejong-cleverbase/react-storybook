71a2486ac956d204a351aa871fb04c0f3b5a7557
Haiko de Jong <haiko.de.jong@cleverbase.com>
2021-04-16 15:43:59 +0200

    chore: click me

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -15,7 +15,7 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Click me, you won\'t regret it.',
 };
 
 export const Secondary = Template.bind({});
